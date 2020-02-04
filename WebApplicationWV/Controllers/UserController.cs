ac1b1b3643f6db96890fa9869411b09a26d0020b
stefa <stefa@LAPTOP-VBQKGM44>
2020-02-04 09:03:09 +0100

    Add project files.

---
+++ WebApplicationWV/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplicationWV.Models;
+
+namespace WebApplicationWV.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly UserDbContext _context;
+
+        public UserController(ILogger<UserController> logger, UserDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            List<User> users = _context.getUsers();
+            return Ok(users);
+        }
+    }
+}
---
+++ WebApplicationWV/Models/UserDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationWV.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+            LoadDefaultUsers();
+        }
+
+        public List<User> getUsers() => Users.Local.ToList<User>();
+        //dit is een verandering voor de github sync
+        private void LoadDefaultUsers()
+        {
+            Users.Add(new User { UserId = 100L, DateOfBirth = DateTime.Now, FirstName = "Stefano" });
+            Users.Add(new User { UserId = 200L, DateOfBirth = DateTime.Now, FirstName = "Alpha" });
+        }
+    }
+}
